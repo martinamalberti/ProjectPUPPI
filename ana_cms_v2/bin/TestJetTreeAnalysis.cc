79a2d336039fa14987b0cbdf08ad1b4a42b215f0
Martina <martina.malberti@cern.ch>
2014-05-15 13:06:33 +0200

    small update

--- ana_cms_v2/bin/TestJetTreeAnalysis.cc
+++ ana_cms_v2/bin/TestJetTreeAnalysis.cc
@@ -5,7 +5,6 @@
 // MAIN                                                                                                                                                                       
 //-------------------------------------------------------                                                                                                                                  
   
-
 int main( int argc, char **argv ) {
 
   gROOT->ProcessLine("#include <vector>");
@@ -22,35 +21,35 @@ int main( int argc, char **argv ) {
   TTree *tree_puppi = (TTree *)inputFile->Get("tree_pf_puppi");
 
   int maxEntries = -1;
+  float minpt = 25.;
+
 
   JetTreeAnalyzer *genAnalyzer = new JetTreeAnalyzer(tree_gen);
-  //  genAnalyzer->bookHistograms("_gen");
-  genAnalyzer->bookHistograms("");
-  genAnalyzer->fillHistograms(maxEntries,25.);
-
+  genAnalyzer->bookHistograms("_gen");
+  //genAnalyzer->bookHistograms("");
+  genAnalyzer->fillHistograms(maxEntries,minpt);
 
   JetTreeAnalyzer *pfAnalyzer = new JetTreeAnalyzer(tree_pf);
-  //  pfAnalyzer->bookHistograms("_pf");
-  pfAnalyzer->bookHistograms("");
-  pfAnalyzer->fillHistograms(maxEntries,25.);
-
-
+  pfAnalyzer->bookHistograms("_pf");
+  //pfAnalyzer->bookHistograms("");
+  pfAnalyzer->fillHistograms(maxEntries,minpt);
+  
   JetTreeAnalyzer *pfchsAnalyzer = new JetTreeAnalyzer(tree_pfchs);
-  //  pfchsAnalyzer->bookHistograms("_pfchs");
-  pfchsAnalyzer->bookHistograms("");
-  pfchsAnalyzer->fillHistograms(maxEntries,25.);
-
+  pfchsAnalyzer->bookHistograms("_pfchs");
+  //pfchsAnalyzer->bookHistograms("");
+  pfchsAnalyzer->fillHistograms(maxEntries,minpt);
 
   JetTreeAnalyzer *puppiAnalyzer = new JetTreeAnalyzer(tree_puppi);
-  //  puppiAnalyzer->bookHistograms("_puppi");
-  puppiAnalyzer->bookHistograms("");
-  puppiAnalyzer->fillHistograms(maxEntries,25.);
+  puppiAnalyzer->bookHistograms("_puppi");
+  //puppiAnalyzer->bookHistograms("");
+  puppiAnalyzer->fillHistograms(maxEntries,minpt);
   
-
   // save results in file
   TFile *outfile = new TFile("out.root","RECREATE");
   genAnalyzer->saveHistograms(outfile,"gen");
   pfAnalyzer->saveHistograms(outfile,"pf");
   pfchsAnalyzer->saveHistograms(outfile,"pfchs");
   puppiAnalyzer->saveHistograms(outfile,"puppi");
+  
+
 }
